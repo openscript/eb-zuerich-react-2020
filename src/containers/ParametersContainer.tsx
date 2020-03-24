5e945aeed98c6120c8ded992eab18045dc63c8b2
Robin Bühler <public@openscript.ch>
2020-03-24 20:19:46 +0100

    Add routing

---
+++ src/containers/ParametersContainer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams, useRouteMatch, useHistory } from "react-router";
+
+interface RouteParams {
+    id: string;
+}
+
+export const ParametersContainer: React.FC = () => {
+    const routeParams = useParams<RouteParams>();
+    const history = useHistory();
+    if(routeParams.id === '5') {
+        history.push('/users')
+    }
+    return <div>{routeParams.id}</div>;
+}
---
+++ src/containers/UsersContainer.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { UserIndex } from "../components/user/UserIndex";
+import { User } from "../models/user";
+import { Gender } from "../models/gender";
+import { UserForm } from "../components/user/UserForm";
+
+export const UsersContainer: React.FC = () => {
+  const defaultUsers: User[] = [
+    {
+      id: 0,
+      forename: "Juck",
+      surname: "Norris",
+      birthday: new Date().getTime(),
+      email: "juck@norris.com",
+      gender: Gender.MALE
+    },
+    {
+      id: 1,
+      forename: "Robin",
+      surname: "Bühler",
+      birthday: new Date().getTime(),
+      email: "r@obin.ch",
+      gender: Gender.MALE
+    },
+    {
+      id: 2,
+      forename: "Scarlett",
+      surname: "Johanson",
+      birthday: new Date().getTime(),
+      email: "s@johannson.ch",
+      gender: Gender.MALE
+    }
+  ];
+
+  const [users, setUsers] = useState<User[]>(defaultUsers);
+
+  const createUser = (user: User) => {
+    const newUser = { ...user, id: new Date().getTime() };
+    const newUsers = [...users, newUser];
+    setUsers(newUsers);
+  };
+
+  const updateUser = (user: User) => {
+    const newUsers = users.map(u => (u.id === user.id ? user : u));
+    setUsers(newUsers);
+  };
+
+  const deleteUser = (userId: number) => {
+    const newUsers = users.filter(u => u.id !== userId);
+    setUsers(newUsers);
+  };
+
+  return (
+    <>
+      <UserForm saveUser={createUser} />
+      <UserIndex
+        users={users}
+        updateAction={updateUser}
+        deleteAction={deleteUser}
+      />
+    </>
+  );
+};
